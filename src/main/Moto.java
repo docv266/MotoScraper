1e805a68486a9a8580e32cf1a85f5c4fdd8631db
docv266 <docv266@gmail.com>
2015-09-15 16:34:37 +0200

    Finalisation

---
+++ src/main/Moto.java
@@ -0,0 +1,148 @@
+package main;
+
+public class Moto
+{
+	private String Modele;
+	private String Marque;
+	private String Annee;
+	private String Genre;
+	private String Cylindree;
+	private String TypeMoteur;
+	private String Puissance;
+	private String Couple;
+	private String TransmissionFinale;
+	private String NombreDeRapports;
+	private String Poids;
+	private String VolumeReservoir;
+	private String HauteurSelle;
+
+	public String getModele()
+	{
+		return Modele;
+	}
+
+	public void setModele(String modele)
+	{
+		Modele = modele;
+	}
+
+	public String getMarque()
+	{
+		return Marque;
+	}
+
+	public void setMarque(String marque)
+	{
+		Marque = marque;
+	}
+
+	public String getAnnee()
+	{
+		return Annee;
+	}
+
+	public void setAnnee(String annee)
+	{
+		Annee = annee;
+	}
+
+	public String getGenre()
+	{
+		return Genre;
+	}
+
+	public void setGenre(String genre)
+	{
+		Genre = genre;
+	}
+
+	public String getCylindree()
+	{
+		return Cylindree;
+	}
+
+	public void setCylindree(String cylindree)
+	{
+		Cylindree = cylindree;
+	}
+
+	public String getTypeMoteur()
+	{
+		return TypeMoteur;
+	}
+
+	public void setTypeMoteur(String typeMoteur)
+	{
+		TypeMoteur = typeMoteur;
+	}
+
+	public String getPuissance()
+	{
+		return Puissance;
+	}
+
+	public void setPuissance(String puissance)
+	{
+		Puissance = puissance;
+	}
+
+	public String getCouple()
+	{
+		return Couple;
+	}
+
+	public void setCouple(String couple)
+	{
+		Couple = couple;
+	}
+
+	public String getTransmissionFinale()
+	{
+		return TransmissionFinale;
+	}
+
+	public void setTransmissionFinale(String transmissionFinale)
+	{
+		TransmissionFinale = transmissionFinale;
+	}
+
+	public String getNombreDeRapports()
+	{
+		return NombreDeRapports;
+	}
+
+	public void setNombreDeRapports(String nombreDeRapports)
+	{
+		NombreDeRapports = nombreDeRapports;
+	}
+
+	public String getPoids()
+	{
+		return Poids;
+	}
+
+	public void setPoids(String poids)
+	{
+		Poids = poids;
+	}
+
+	public String getVolumeReservoir()
+	{
+		return VolumeReservoir;
+	}
+
+	public void setVolumeReservoir(String volumeReservoir)
+	{
+		VolumeReservoir = volumeReservoir;
+	}
+
+	public String getHauteurSelle()
+	{
+		return HauteurSelle;
+	}
+
+	public void setHauteurSelle(String hauteurSelle)
+	{
+		HauteurSelle = hauteurSelle;
+	}
+}
--- src/main/Telecharger.java
+++ src/main/Telecharger.java
@@ -1,6 +1,8 @@
 package main;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.jsoup.Jsoup;
@@ -28,6 +30,8 @@ public class Telecharger
 			dossier.mkdirs();
 		}
 
+		BufferedWriter writer = new BufferedWriter(new FileWriter(sortie));
+
 		// Parcours du document racine (listant les années)
 		Document docRoot = Jsoup.connect("http://www.bikez.com/years/index.php").get();
 
@@ -47,12 +51,60 @@ public class Telecharger
 
 			for (Element linkModele : modele)
 			{
-				System.out.println(linkModele.attr("abs:href"));
+				Moto maMoto = new Moto();
+
+				// Parcours du document détaillant le modèle
+				Document docMoto = Jsoup.connect(linkModele.attr("abs:href")).get();
+
+				maMoto.setMarque(docMoto.select("h1 a[href]:eq(0)").text().trim());
+
+				maMoto.setAnnee(docMoto.select("h1 a[href]:eq(1)").text().trim());
+
+				maMoto.setModele(docMoto.select("h1").first().childNode(1).toString().trim());
+
+				maMoto.setGenre(docMoto.select("td:contains(Category:) + td").text().trim());
+
+				maMoto.setCylindree(docMoto.select("td:contains(Displacement:) + td").text().trim());
+
+				maMoto.setTypeMoteur(docMoto.select("td:contains(Engine type:) + td").text().trim());
+
+				maMoto.setPuissance(docMoto.select("td:contains(Power:) + td").text().trim());
+
+				maMoto.setCouple(docMoto.select("td:contains(Torque:) + td").text().trim());
+
+				maMoto.setTransmissionFinale(docMoto.select("td:contains(final drive:) + td").text().trim());
+
+				maMoto.setNombreDeRapports(docMoto.select("td:contains(Gearbox:) + td").text().trim());
+
+				maMoto.setPoids(docMoto.select("td:contains(Weight incl. oil, gas,) + td").text().trim());
+
+				maMoto.setVolumeReservoir(docMoto.select("td:contains(Fuel capacity:) + td").text().trim());
+
+				maMoto.setHauteurSelle(docMoto.select("td:contains(Seat height:) + td").text().trim());
+
+				writer.write(maMoto.getMarque() + ";");
+				writer.write(maMoto.getModele() + ";");
+				writer.write(maMoto.getAnnee() + ";");
+				writer.write(maMoto.getCylindree() + ";");
+				writer.write(maMoto.getGenre() + ";");
+				writer.write(maMoto.getTypeMoteur() + ";");
+				writer.write(maMoto.getPuissance() + ";");
+				writer.write(maMoto.getCouple() + ";");
+				writer.write(maMoto.getTransmissionFinale() + ";");
+				writer.write(maMoto.getNombreDeRapports() + ";");
+				writer.write(maMoto.getPoids() + ";");
+				writer.write(maMoto.getVolumeReservoir() + ";");
+				writer.write(maMoto.getHauteurSelle());
+
+				writer.newLine();
+
 			}
 
-			break;
-
 		}
+
+		writer.close();
+
+		System.out.println("Fin");
 	}
 
 }
