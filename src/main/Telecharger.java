d950c7029c1411c77a1345e8f3b92db37e82a1d0
docv266 <docv266@gmail.com>
2015-09-15 14:52:03 +0200

    Changement de site cible
    Complete rewrite

--- src/main/Telecharger.java
+++ src/main/Telecharger.java
@@ -1,14 +1,12 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Telecharger
 {
@@ -30,119 +28,31 @@ public class Telecharger
 			dossier.mkdirs();
 		}
 
-		if (true)
+		// Parcours du document racine (listant les années)
+		Document docRoot = Jsoup.connect("http://www.bikez.com/years/index.php").get();
+
+		// On récupère les éléments faisant partie de l'élément ayant pour classe "zebra", de type "a", avec un attribut "href" et contenant exactement le texte "list view"
+		Elements table = docRoot.select(".zebra a[href]:containsOwn(list view)");
+
+		for (Element linkAnnee : table)
 		{
 
-			// Téléchargement des fichiers absents
-			for (int i = 1; i < 1718; i++)
+			// Parcours du document listant les modèle pour une année donnée.
+			Document docAnnee = Jsoup.connect(linkAnnee.attr("abs:href")).get();
+
+			// On récupère les éléments faisant partie de l'élément ayant pour classe "zebra", de type "a", avec un attribut "href".
+			// Puis on retire les éléments "a" qui contiennent un élément "img" (On retire ainsi les lien vers les annonces contenant une image, toutes les annonces n'ont pas nécessairement une image)
+			// Puis on retire les éléments "a" qui ont un attribut "href" commençant par "/brand/" (On retire ainsi les lien vers la marque)
+			Elements modele = docAnnee.select(".zebra a[href]").not("a:has(img)").not("a[href^=/brand/]");
+
+			for (Element linkModele : modele)
 			{
-
-				boolean ATraiter = true;
-
-				for (File fichier : dossier.listFiles())
-				{
-					if (fichier.getName().equals(i + ".txt"))
-					{
-						ATraiter = false;
-						break;
-					}
-				}
-
-				if (ATraiter)
-				{
-
-					URL adresse = new URL("http://www.motoprogress.com/fiche-moto.php?id_moto=" + i);
-
-					BufferedReader in = new BufferedReader(new InputStreamReader(adresse.openStream()));
-
-					BufferedWriter writer = new BufferedWriter(new FileWriter("save" + File.separator + i + ".txt"));
-
-					String inputLine;
-					while ((inputLine = in.readLine()) != null)
-					{
-						try
-						{
-							writer.write(inputLine);
-							writer.newLine();
-						}
-						catch (IOException e)
-						{
-							e.printStackTrace();
-						}
-					}
-
-					in.close();
-					writer.close();
-				}
+				System.out.println(linkModele.attr("abs:href"));
 			}
 
+			break;
+
 		}
-
-		Scanner sc;
-		BufferedWriter writer = new BufferedWriter(new FileWriter(sortie));
-		boolean FlagGenre = false;
-		boolean FlagMarque = false;
-		boolean FlagCylindree = false;
-		boolean FlagModele = false;
-
-		// Parcours des fichiers
-		for (File fichier : dossier.listFiles())
-		{
-			sc = new Scanner(new BufferedReader(new FileReader(fichier)));
-			while (sc.hasNext())
-			{
-				String line = sc.nextLine();
-
-				// Ecriture
-				if (FlagGenre)
-				{
-					writer.write(line.substring(line.indexOf("<td>") + 4, line.indexOf("</td>")).trim() + ",");
-					FlagGenre = false;
-				}
-				if (FlagMarque)
-				{
-					writer.write(line.substring(line.indexOf("<td>") + 4, line.indexOf("</td>")).replace("<br />", "")
-							+ ",");
-					FlagMarque = false;
-				}
-				if (FlagCylindree)
-				{
-					writer.write(
-							line.substring(line.indexOf("<td>") + 4, line.indexOf("</td>")).replace("  cm&sup3", ""));
-					FlagCylindree = false;
-				}
-				if (FlagModele)
-				{
-					writer.write(line.substring(line.indexOf("<td>") + 4, line.indexOf("</td>")).replace("<br />", "")
-							+ ",");
-					FlagModele = false;
-				}
-
-				// Flag
-				if (line.contains("<td>Type : </td>"))
-				{
-					FlagGenre = true;
-				}
-				if (line.contains("<td>Marque : </td>"))
-				{
-					FlagMarque = true;
-				}
-				if (line.contains("<td>Cylindr&eacute;e : </td>"))
-				{
-					FlagCylindree = true;
-				}
-				if (line.contains("<td>Modele : </td>"))
-				{
-					FlagModele = true;
-				}
-
-			}
-			writer.newLine();
-		}
-
-		writer.close();
-
-		System.out.println("Fin.");
 	}
 
 }
